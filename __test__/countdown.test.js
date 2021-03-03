4b221cc252b461d50869ca44c892c1f4bd189e53
jessiek208 <73068180+jessiek208@users.noreply.github.com>
2021-03-03 12:27:31 -0500

    Updates/fixes countdown test

--- __test__/countdown.test.js
+++ __test__/countdown.test.js
@@ -1,17 +1,9 @@
-import { countdown } from '../src/client/js/countdown'
+import { countDown } from '../src/client/js/countdown'
 
-import { advanceBy, advanceTo, clear } from 'jest-date-mock';
- 
-advanceTo(0); // reset to timestamp = 0
- 
-Date.now(); // will got 0
- 
-Date.current(); // will got the actual timestamp.
+import "babel-polyfill"
 
-describe("Test countdown function", () => {
-    test('Return 3 days', () => {
-        
-        expect(countdown('2021-03-04')).toReturn(7)
+describe("test countDown function", () => {
+    test('that countDown is not defined when given an empty argument', ()  => {
+        expect(countDown).toBeUndefined;
     })
 })
-
